43e2fb139e61016f519a375a227cd41d4d48c8a3
KHUSHI 444 <147485713+khushi4452@users.noreply.github.com>
2024-06-24 21:57:27 +0530

    Update Notes.js

--- Models/Notes.js
+++ Models/Notes.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
 const noteSchema = new Schema({
     title: String,
     content: String,
-    category: String
+    category: String,
+    name: string
 });
 
 const Note = mongoose.model("Note", noteSchema);
